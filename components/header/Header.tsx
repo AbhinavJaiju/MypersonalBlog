0258d5fa765d536814c51e5670a3030810902aae
Abhinav-jaiju <abhinav.jaiju@experionglobal.com>
2022-12-08 18:37:45 +0530

    final final push

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
                 <div className="md:float-left block">
                     <Link href={"/"}>
                         <span className="cursor-pointer font-bold text-4xl text-white">
-                            Snowman's Mind
+                            Clueless Engineer
                         </span>
                     </Link>
                 </div>
