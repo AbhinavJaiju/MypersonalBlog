4590f9991e7c6e45fa8b025da8dc9a7702f0596e
Abhinav-jaiju <abhinav.jaiju@experionglobal.com>
2022-12-08 18:05:17 +0530

    comiting

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,52 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { useRouter } from 'next/router';
 
-import { getPosts, getPostDetails } from '../../services'
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
+import { getPosts, getPostDetails } from '../../services';
+import { AdjacentPosts } from '../../section';
 
-import { PostDetail, Categories, Author, Comments, CommentsForm, PostWidget } from '../../components'
-import AdjacentPosts from '../../section/AdjacentPosts'
+const PostDetails = ({ post  } :any) => {
+  const router = useRouter();
 
-const PostDetails = ({ post }: any) => {
-    return (
-        <>
-            <div className="container mx-auto px-10 mb-8">
-                <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-                    <div className="col-span-1 lg:col-span-8">
-                        <PostDetail post={post} />
-                        <Author author={post.author} />
-                        <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
-                        <CommentsForm slug={post.slug} />
-                        <Comments slug={post.slug} />
-                    </div>
-                    <div className="col-span-1 lg:col-span-4">
-                        <div className="relative lg:sticky top-8">
-                            <PostWidget slug={post.slug} categories={post.categories.map((category: any) => category.slug)} />
-                            <Categories />
-                        </div>
-                    </div>
-                </div>
+  if (router.isFallback) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      <div className="container mx-auto px-10 mb-8">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="col-span-1 lg:col-span-8">
+            <PostDetail post={post} />
+            <Author author={post.author} />
+            <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
+            <CommentsForm slug={post.slug} />
+            <Comments slug={post.slug} />
+          </div>
+          <div className="col-span-1 lg:col-span-4">
+            <div className="relative lg:sticky top-8">
+              <PostWidget slug={post.slug} categories={post.categories.map((category :any) => category.slug)} />
+              <Categories />
             </div>
-        </>
-    )
-}
-
-export default PostDetails
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default PostDetails;
 
 // Fetch data at build time
-export async function getStaticProps({ params }: any) {
-    const data = await getPostDetails(params.slug);
-    return {
-        props: {
-            post: data,
-        },
-    };
+export async function getStaticProps({ params } : any) {
+  const data = await getPostDetails(params.slug);
+  return {
+    props: {
+      post: data,
+    },
+  };
 }
 
 // Specify dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
-    const posts = await getPosts();
-    return {
-        paths: posts.map(({ node: { slug } }: any) => ({ params: { slug } })),
-        fallback: true,
-    };
+  const posts = await getPosts();
+  return {
+    paths: posts.map(({ node: { slug  } } : any) => ({ params: { slug } })),
+    fallback: true,
+  };
 }
