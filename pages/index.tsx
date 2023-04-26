49f5b8f1451f094e302328a39ca3ee876d137702
Abhinav-jaiju <abhinav.jaiju@experionglobal.com>
2023-04-27 00:45:40 +0530

    arrow UI change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { PostCard, PostWidget, Categories, Author} from '../components'
+import { PostCard, PostWidget, Categories, Author } from '../components'
 import { getPosts } from '../services'
 import { FeaturedPosts } from '../section'
 
 
-const Home: NextPage = ({ posts }:any) => {
+const Home: NextPage = ({ posts }: any) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -15,18 +15,18 @@ const Home: NextPage = ({ posts }:any) => {
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className='lg:col-span-8 col-span-1'>
-        {posts.map((post:any,index : any)=>{
-          return(
-            <PostCard post={post.node} key={index} />
-          )
-        })}
+          {posts.map((post: any, index: any) => {
+            return (
+              <PostCard post={post.node} key={index} />
+            )
+          })}
         </div>
-      <div className="lg:col-span-4 col-span-1">
-        <div className="lg:sticky relative top-8">
-          <PostWidget />
-          <Categories />
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
-      </div>
       </div>
     </div>
   )
@@ -34,9 +34,9 @@ const Home: NextPage = ({ posts }:any) => {
 
 export default Home
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   const posts = (await getPosts()) || [];
   return {
-    props : {posts}
+    props: { posts }
   }
 }
